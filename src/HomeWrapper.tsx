891dcf0d5e05cf7f65447dfc51961b46c67d6ef5
Minho Jang <166613620+miinhho@users.noreply.github.com>
2025-08-18 18:39:04 +0900

    Update HomeWrapper.tsx

--- src/HomeWrapper.tsx
+++ src/HomeWrapper.tsx
@@ -1,14 +1,12 @@
 import { Link } from 'react-router';
 import styles from './HomeWrapper.module.css';
 
-const HomeWrapper = (children: React.ReactNode) => {
-  return () =>
+const HomeWrapper = (children: React.ReactNode) => () =>
   (
     <div className={styles.container}>
       <Link to="/" className={styles.homeButton} title="홈으로 이동">🏠</Link>
       {children}
     </div>
   )
-}
 
 export default HomeWrapper
